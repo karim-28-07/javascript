a54952f4772af91e2d78467bc86a0694a5d7fc9e
karim-07-28 <karim.mezouar28@gmail.com>
2021-03-08 16:04:11 +0100

    Javascript

---
+++ jour-1/jour01.js
@@ -0,0 +1,32 @@
+//console.log("Hello World !");
+
+// String
+var test = "My name is Karim";
+//console.log("test");
+
+// Concatenation 
+var name = "Karim";
+//console.log("Nice to meet you" + " " + "Karim");
+
+// String Length
+var testLength = "I'm very long !";
+//console.log(testLength.length);
+
+// Replace 
+var food = "croissant is meh";
+food = food.replace("meh", "so good");
+//console.log(food);
+
+//Up and Down 
+var basic = "This is Cool";
+var basicUp = ("THIS IS COOL");
+var basicDown = ("this is cool");
+// console.log(basic);
+// console.log(basicUp);
+// console.log(basicDown);
+
+// Split 
+
+var word = "banana";
+var letters = word.split("banana");
+colsole.log(letters);
