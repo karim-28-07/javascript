55d1b0d9c42723dc4cd38e36aee30b7c7995411d
karim-07-28 <karim.mezouar28@gmail.com>
2021-03-08 17:32:18 +0100

    javascript

--- jour-1/jour01.js
+++ jour-1/jour01.js
@@ -2,34 +2,34 @@
 
 // String
 var test = "My name is Karim";
-//console.log("test");
+console.log(test);
 
 // Concatenation 
 var name = "Karim";
-//console.log("Nice to meet you" + " " + "Karim");
+console.log("Nice to meet you" + " " + "Karim");
 
 // String Length
 var testLength = "I'm very long !";
-//console.log(testLength.length);
+console.log(testLength.length);
 
 // Replace 
 var food = "croissant is meh";
 food = food.replace("meh", "so good");
-//console.log(food);
+console.log(food);
 
 //Up and Down 
 var basic = "This is Cool";
 var basicUp = ("THIS IS COOL");
 var basicDown = ("this is cool");
-// console.log(basic);
-// console.log(basicUp);
-// console.log(basicDown);
+console.log(basic);
+console.log(basicUp);
+console.log(basicDown);
 
 // Split 
 
 var word = "banana";
 var letters = word.split("");
-//console.log(letters);
+console.log(letters);
 
 // Template 
 
