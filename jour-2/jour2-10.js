6b4d31356c853db2c35a1489c398311e861124cc
karim-07-28 <karim.mezouar28@gmail.com>
2021-03-09 16:28:18 +0100

    Exercices Jour-2 Javascript

---
+++ jour-2/jour2-10.js
@@ -0,0 +1,57 @@
+// Bonus II
+
+var month = "June";
+
+switch (month) {
+    case "January" : 
+    console.log("Winter");
+    break;
+    
+    case "February" :
+    console.log("Winter");
+    break;
+
+    case "March" :
+    console.log("Spring");
+    break;
+
+    case "April" :
+    console.log("Spring");
+    break;
+
+    case "May" :
+    console.log("Spring");
+    break;
+
+    case "June" :
+    console.log("Summer");
+    break;
+
+    case "July" :
+    console.log("Summer");
+    break;
+
+    case "August" :
+    console.log("Summer");
+    break;
+
+    case "September" :
+    console.log("Winter");
+    break;
+
+    case "October" :
+    console.log("Winter");
+    break;
+
+    case "Novenber" :
+    console.log("Winter");
+    break;
+
+    case "December" :
+    console.log("Winter");
+    break;
+
+    default :
+    console.log("Fall");
+
+}
--- jour-2/jour2-9.js
+++ jour-2/jour2-9.js
@@ -3,7 +3,13 @@
 var min = 1;
 var max = 6;
 
-if (Math.floor (Math.random() * (max - min + 1) + min)) {
-    console.log("Yes I win");
-} else (Math.floor (Math.random() * (100 - 20 + 1) + 20)) 
-    console.log("So close...");
+var random = Math.floor(1 + Math.random() * 6)
+
+console.log(random)
+
+if (random != 6) {
+    console.log("so close");
+} else {
+    console.log("Yes I Win");
+
+}
