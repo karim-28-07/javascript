b0666e982d14770bfce24792777360b2cbed881c
karim-07-28 <karim.mezouar28@gmail.com>
2021-03-09 17:28:41 +0100

    Exercices jour-2 Javascript

---
+++ jour-2/jour2-11.js
@@ -0,0 +1,10 @@
+// Bonus III
+
+var roundedNumber = 3.6;
+Math.floor(roundedNumber);
+Math.ceil(roundedNumber);
+
+if (Math.ceil(roundedNumber) > 3.6){
+    console.log(Math.ceil);
+}else (Math.floor(roundedNumber) < 3.6) 
+    console.log(Math.floor);
