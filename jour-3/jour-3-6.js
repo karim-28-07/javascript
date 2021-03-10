d043baee7a9dbade35e6aad6008bb39a904054fb
karim-07-28 <karim.mezouar28@gmail.com>
2021-03-10 15:57:53 +0100

    Exercices jour-3 Javascript

---
+++ jour-3/jour-3-6.js
@@ -0,0 +1,9 @@
+// Reverse
+
+var sentence = "Hello Konexio !";
+
+for (var i = 0; i = "Hello Konexio !"; i--){
+    console.log(i);
+}
+
+console.log(sentence);
---
+++ jour-3/jour-3-bonus2.js
@@ -0,0 +1,13 @@
+// Bonus 2
+
+var total = 0;
+var limit = 10;
+
+while (total <= limit) {
+    total++;
+}
+
+// for (var i = 0; i <= limit; i++) {
+    // console.log(total+=i)
+    
+// }
