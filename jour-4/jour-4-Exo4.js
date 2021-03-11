1db38fd3ef8e1856a3c0a000990f9b0f601b0ef3
karim-07-28 <karim.mezouar28@gmail.com>
2021-03-11 16:56:43 +0100

    Exercices jour-4 Javascript

--- jour-4/jour-4-Exo4.js
+++ jour-4/jour-4-Exo4.js
@@ -1,6 +1,25 @@
 // Compare
+var number1 = 200;
+var number2 = 200;
 
-function Compare(num1, num2){
+function Compare(num1, num2) {
+    
+   
+    if (number1 > number2) {
+        console.log("num1 is bigger");
+    } 
+    else if (number1 < number2) {
+        console.log("num2 is bigger");
+    } 
+    else {
+        console.log("both are the same");
+    }
 
 }
+
+Compare()
+
+
+
+
 
---
+++ jour-4/jour-4-Exo5.js
@@ -0,0 +1,15 @@
+// Add Up
+var min = 0;
+var max = 12;
+
+function addUp (num){
+
+    for (var i = 0; i <=min ; i++) {
+        min+= i;
+        console.log(min);
+    }
+
+}
+
+    
+addUp()
